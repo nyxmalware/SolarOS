bd9f3ae15c0227b4782e120bd0ed065dd18b1c94
Nyx <nyxmalware@gmail.com>
2026-06-01 18:05:14 +0300

    Refactor Makefile to use object files instead of binaries

--- makefile.mk
+++ makefile.mk
@@ -1,29 +1,27 @@
-# SolarOS Makefile
-# usage: make build, make run, make clean
-
 NASM = nasm
-QEMU = qemu-system-x86_64
+NASMFLAGS = -f elf32
+LD = ld
+LDFLAGS = -T link.ld -m elf_i386
+QEMU = qemu-system-i386
 IMG = solaros.img
 
-SOURCES = boot.asm initrix.asm kernel.asm
-OBJECTS = boot.bin initrix.bin kernel.bin
+ASM_OBJS = boot.o initrix.o kernel.o
 
 all: build run
 
-build: $(OBJECTS)
+build: $(ASM_OBJS)
+	$(LD) $(LDFLAGS) -o kernel.bin $(ASM_OBJS)
 	dd if=/dev/zero of=$(IMG) bs=512 count=2880 status=none
 	dd if=boot.bin of=$(IMG) conv=notrunc status=none
-	dd if=initrix.bin of=$(IMG) seek=2 conv=notrunc status=none
-	dd if=kernel.bin of=$(IMG) seek=4 conv=notrunc status=none
-	@echo "Build complete: $(IMG)"
+	dd if=kernel.bin of=$(IMG) seek=2 conv=notrunc status=none
 
-%.bin: %.asm
-	$(NASM) -f bin $< -o $@
+%.o: %.asm
+	$(NASM) $(NASMFLAGS) $< -o $@
 
 run:
 	$(QEMU) -fda $(IMG)
 
 clean:
-	rm -f $(OBJECTS) $(IMG)
+	rm -f *.o *.bin $(IMG)
 
 .PHONY: all build run clean
