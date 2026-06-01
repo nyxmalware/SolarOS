37105b2db7d0d70fef575f2bcd442f5cad6c2ff0
Nyx <nyxmalware@gmail.com>
2026-06-01 16:52:48 +0300

    Update kernel.asm for version 1.1 enhancements

--- kernel.asm
+++ kernel.asm
@@ -1,4 +1,4 @@
-; SolarOS Kernel v1.0
+; SolarOS Kernel v1.1
 ; 16-бит реальный режим
 ; точка входа из initrix.asm
 
@@ -6,66 +6,60 @@ org 0x0000
 bits 16
 
 start:
-    ; настройка сегментов данных
     mov ax, cs
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0xFFF0
 
-    ; сохраняем параметры от загрузчика
     mov [boot_drive], dl
 
-    ; очищаем экран
     mov ax, 0x0003
     int 0x10
 
-    ; выводим заставку
     mov si, logo_solar
     call print
 
-    ; инициализация прерываний
     call init_idt
     call init_irq
-
-    ; инициализация клавиатуры
     call keyboard_init
+    call setup_keyboard_int
 
-    ; инициализация мыши (если есть)
-    call mouse_init
-
-    ; инициализация видеорежима (графика или текст)
-    call vga_init
-
-    ; главный цикл
 main_loop:
     mov si, prompt
     call print
-
     call input_line
     call parse_command
-
     call print_newline
     jmp main_loop
 
-; включение файлов модулей
 %include "kernel/print.asm"
 %include "kernel/input.asm"
+%include "kernel/commands.asm"
 %include "kernel/idt.asm"
 %include "kernel/keyboard.asm"
-%include "kernel/commands.asm"
+%include "disk/read.asm"
+%include "disk/disk_params.asm"
+%include "disk/fat12.asm"
 
-; видеорежим
-vga_init:
+setup_keyboard_int:
     pusha
-    mov ax, 0x0003    ; текст 80x25
-    int 0x10
+    cli
+    mov word [0x24], keyboard_handler
+    mov word [0x26], 0x0000
+    sti
     popa
     ret
 
-; данные
+error_handler:
+    call print
+    mov ah, 0
+    int 0x16
+    jmp 0xFFFF:0
+
 boot_drive:    db 0
 prompt:        db 'SolarOS@User >> ', 0
+
 logo_solar:    
     db 0x0D, 0x0A
     db '   SSS   OOO   L       AAA    RRR    OOO   SSS  ', 0x0D, 0x0A
@@ -74,10 +68,3 @@ logo_solar:
     db '      S O   O  L      A   A   R  R   O   O     S ', 0x0D, 0x0A
     db '  SSS   OOO   LLLLL  A   A   R   R   OOO   SSS   ', 0x0D, 0x0A
     db 0x0D, 0x0A, 'SolarOS v1.0 (C) 2026', 0x0D, 0x0A, 0x0D, 0x0A, 0
-
-; обработчик ошибок
-error_handler:
-    call print          ; si уже содержит сообщение об ошибке
-    mov ah, 0
-    int 0x16            ; ждём нажатие клавиши
-    jmp 0xFFFF:0        ; reboot
