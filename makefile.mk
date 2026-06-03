036d2284b008742cb4dc3aa6257ca01ff7c19575
Nyx <nyxmalware@gmail.com>
2026-06-03 21:32:13 +0300

    Update makefile.mk

--- makefile.mk
+++ makefile.mk
@@ -1,32 +1,23 @@
 NASM = nasm
-RUSTC = cargo
-LD = ld
 QEMU = qemu-system-x86_64
 IMG = solaros.img
 
-ASM_OBJS = initrix.o kernel.o
-
-all: build_asm build_rust link build_img
-
-build_asm:
-	$(NASM) -f bin boot.asm -o boot.bin
-	$(NASM) -f elf32 initrix.asm -o initrix.o
-	$(NASM) -f elf32 kernel.asm -o kernel.o
-
-build_rust:
-	cd rust && $(RUSTC) build --release
-
-link:
-	$(LD) -m elf_i386 -T link.ld -o solaros.bin initrix.o kernel.o rust/target/release/libsolaros_rust.a
+all: build_img
 
 build_img:
-	cat boot.bin solaros.bin > $(IMG)
-
-clean:
-	rm -f boot.bin *.o *.bin *.img
-	cd rust && $(RUSTC) clean
+	$(NASM) -f bin boot.asm -o boot.bin
+	$(NASM) -f bin initrix.asm -o initrix.bin
+	$(NASM) -f bin kernel.asm -o kernel.bin
+	dd if=/dev/zero of=$(IMG) bs=512 count=2880 status=none
+	dd if=boot.bin of=$(IMG) conv=notrunc status=none
+	dd if=initrix.bin of=$(IMG) seek=2 conv=notrunc status=none
+	dd if=kernel.bin of=$(IMG) seek=4 conv=notrunc status=none
+	@echo "Build complete: $(IMG)"
 
 run:
 	$(QEMU) -fda $(IMG)
 
-.PHONY: all build_asm build_rust link build_img clean run
+clean:
+	rm -f boot.bin initrix.bin kernel.bin $(IMG)
+
+.PHONY: all build_img run clean
